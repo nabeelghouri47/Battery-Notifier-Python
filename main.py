f818ec4b36e0e04d732c6b991bfbe356e0130d8e
Nabeel ghouri <nabeelahmedkhan47@gmail.com>
2022-10-18 02:24:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,29 @@ import win32com.client as wincl
 from plyer import notification
 import psutil
 
-class Batterycheaker:
 
-    #this function is convert battery reaming time into hour and minutes
+class Batterycheaker:
+    # this function is convert battery reaming time into hour and minutes
     def convertTime(self, seconds):
         minutes, seconds = divmod(seconds, 60)
         hours, minutes = divmod(minutes, 60)
-        return "%d hour %02d minutes %02d" % (hours, minutes, seconds)
+        return "%d hour %02d minutes %02d second" % (hours, minutes, seconds)
 
-    #this function will return the actuall time period AM or PM and help to say correct welcome message
-    def currentTimeperiod(self):
-        timePeriod = datetime.today().strftime("%I:%M %p")
-        if "AM" in timePeriod:
-            return "AM"
+    # this function will return the actuall time period AM or PM and help to say correct welcome message
+    def partofDay(self, x):
+        if (x > 4) and (x <= 8):
+            return 'Early Morning'
+        elif (x > 8) and (x <= 12):
+            return 'Morning'
+        elif (x > 12) and (x <= 16):
+            return 'After Noon'
+        elif (x > 16) and (x <= 20):
+            return 'Good Evening'
+        elif (x > 20) and (x <= 24):
+            return 'Good Night'
         else:
-            return "PM"
-
-    #function is to speak
+            return 'Late Night'
+    # function is to speak
     def speak(self, str):
         speaker_number = 1
         spk = wincl.Dispatch("SAPI.SpVoice")
@@ -30,12 +36,13 @@ class Batterycheaker:
         spk.SetVoice(vcs.Item(speaker_number))
         spk.Speak(str)
 
-    #function to battery Notification
+    # function to battery Notification
     def Battery_Notification(self):
-        battery = psutil.sensors_battery()
-        self.battery_percent = battery.percent;
-        battery_plugged = battery.power_plugged
-        message = str(self.battery_percent) + "% Battery Remaining \n"+"power plugged is "+ str(battery_plugged)+"\n"+str(self.convertTime(battery.secsleft))
+        self.battery = psutil.sensors_battery()
+        self.battery_percent = self.battery.percent;
+        self.battery_plugged = self.battery.power_plugged
+        message = str(self.battery_percent) + "% Battery Remaining \n" + "power plugged is " + str(
+            self.battery_plugged) + "\n" + str(self.convertTime(self.battery.secsleft))
         notification.notify(
             app_name="Agent Diana",
             title="Battery Percentage",
@@ -43,25 +50,31 @@ class Batterycheaker:
             timeout=10
         )
 
-    #function to detect your reamainig battery and tell you
+    # function to detect your reamainig battery and tell you
     def batteryNotify(self):
         self.Battery_Notification()
-        if(int(self.battery_percent) >= 90):
-            self.speak("sir your battery is fully charged plugged out your charger")
+        if (int(self.battery_percent) >= 90):
+            self.speak("sir your battery "+str(self.battery_percent)+" is approxtimately full charged so plugged out your charger")
         else:
-            if (int(self.battery_percent) <= 25):
+            if (int(self.battery_percent) <= 20):
                 self.speak("sir your battery is low plugged in your charger")
             else:
-                self.speak("sorry for disturbing you sir")
+                self.speak(
+                    "your remaining battery is " + str(self.battery_percent) + " percent which you can use for " + str(
+                        self.convertTime(self.battery.secsleft)) + " left")
 
-
-#and here is main function where yout program execution startup
+    def checkBatteryplugged(self):
+        bateryPlugged = False
+        if(self.battery_plugged == True):
+            bateryPlugged = True
+        return bateryPlugged
+# and here is main function where yout program execution startup
 if __name__ == "__main__":
     batteryCheaker_Obj = Batterycheaker()
-    if batteryCheaker_Obj.currentTimeperiod() == "AM":
-        batteryCheaker_Obj.speak("welcome sir Diana here the is "+datetime.today().strftime("%I:%M %p")+" now that's why i'm saying good night")
-    else:
-        batteryCheaker_Obj.speak("welcome sir Diana here the is " + datetime.today().strftime("%I:%M %p") + " now and good morning")
-    while(True):
+    currentTime = datetime.now()
+    hour = currentTime.hour
+    batteryCheaker_Obj.speak(batteryCheaker_Obj.partofDay(hour) + " Code 47 diana here time is: "+datetime.today().strftime("%I:%M:%p"))
+
+    while (True):
         batteryCheaker_Obj.batteryNotify()
-        time.sleep(60*15)
+        time.sleep(60 * 15)
